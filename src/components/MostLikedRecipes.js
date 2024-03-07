7a35065b1824de3f35c0ac6b39c59f70ce1368c6
Fidaa Hammoud <fidaa.hammoud@profitisle.com>
2024-03-07 20:06:52 +0200

    Add sorting option first test

---
+++ src/components/MostLikedRecipes.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { API_HOST } from "@env";
+import { useAuth } from '../components/AuthProvider';
+import HttpService from './HttpService';
+import { Utils } from './Utils'; 
+
+const BASE_URL = 'http://192.168.56.10:80/laravel';
+
+const MostLikedRecipes = () => {
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const [recipes, setRecipes] = useState([]);
+  const { getAuthData } = useAuth();
+  const { token } = getAuthData();
+  const [error, setError] = useState(null);
+
+  
+  const { getTimeDifference } = Utils();
+
+  useEffect(() => {
+    const fetchLatestRecipes = async () => {
+      try {
+        const httpService = new HttpService();
+        const response = await httpService.get(`${API_HOST}/recipes?sort=-totalLikes`, null);
+        setRecipes(response.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchLatestRecipes();
+  }, [isFocused]);
+
+  const handleRecipePress = (recipeId) => {
+    navigation.navigate('RecipeDetails', { recipeId });
+  };
+
+  if (error) {
+    return <Text>Error fetching latest recipes: {error}</Text>;
+  }
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {recipes.map((recipe) => (
+        <TouchableOpacity key={recipe.id} style={styles.recipeItem} onPress={() => handleRecipePress(recipe.id)}>
+          <View style={styles.creatorContainer}>
+            <Image source={{ uri: `${BASE_URL}/storage/${recipe.user.images.image}` }} style={styles.creatorImage} />
+            <Text style={styles.creatorName}>{recipe.user.name}</Text>
+          </View>
+          <Image source={{ uri: `${BASE_URL}/storage/${recipe.images.image}` }} style={styles.recipeImage} />
+          <View style={styles.titleContainer}>
+            <Text style={styles.recipeTitle}>{recipe.title}</Text>
+            <Text style={styles.categoryName}>{recipe.category.name}</Text>
+          </View>
+          <View style={styles.recipeDetails}>
+            <View style={styles.likesContainer}>
+              <Icon name="thumbs-o-up" size={20} color="green" style={styles.likesIcon} />
+              <Text style={styles.likesText}>{recipe.totalLikes}</Text>
+            </View>
+            <View style={styles.ratingContainer}>
+              <Icon name="star" size={20} color="gold" style={styles.ratingIcon} />
+              <Text style={styles.ratingText}>{recipe.avrgRating}</Text>
+            </View>
+          </View>
+          {/* Use getTimeDifference function */}
+          <Text style={styles.createdAt}>{getTimeDifference(recipe.created_at)}</Text>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  recipeItem: {
+    marginBottom: 16,
+  },
+  creatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  creatorImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 8,
+  },
+  creatorName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  recipeImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  recipeTitle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  categoryName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  recipeDetails: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  likesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  likesIcon: {
+    marginRight: 5,
+  },
+  likesText: {
+    fontSize: 16,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  ratingIcon: {
+    marginRight: 5,
+  },
+  ratingText: {
+    fontSize: 16,
+  },
+  createdAt: {
+    fontSize: 12,
+    color: 'gray',
+    marginTop: 5,
+  },
+});
+
+export default MostLikedRecipes;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { View, Image, TextInput, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Image, TextInput, Text, StyleSheet, ScrollView, TouchableOpacity, Modal, TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useAuth } from '../components/AuthProvider';
 import Categories from "../components/Categories";
 import Chefs from "../components/Chefs";
 import LatestRecipes from "../components/LatestRecipes";
+import MostLikedRecipes from "../components/MostLikedRecipes";
+import MostAverageRatingRecipes from "../components/MostAverageRatingRecipes";
+import SortRecipeByPreparationTime from "../components/SortRecipeByPreparationTime";
+
+
 import Footer from "../components/Footer"; 
 
 const HomeScreen = () => {
@@ -13,11 +18,38 @@ const HomeScreen = () => {
   const { getAuthData } = useAuth();
   const { userId } = getAuthData();
   const [searchQuery, setSearchQuery] = useState('');
-
+  const [sortingOption, setSortingOption] = useState('latest'); // Default to 'latest'
+  const [modalVisible, setModalVisible] = useState(false);
   const handleSearch = async () => {
     navigation.navigate('SearchResults', { searchQuery });
   };
-  
+
+  const openSortModal = () => {
+    setModalVisible(true);
+  };
+
+  // Function to handle sorting option change
+  const handleSortingOptionChange = (option) => {
+    setSortingOption(option);
+    setModalVisible(false); // Close the modal after selecting sorting option
+  };
+
+  // Function to render the appropriate component based on sorting option
+  const renderRecipesComponent = () => {
+    switch (sortingOption) {
+      case 'latest':
+        return <LatestRecipes />;
+      case 'mostLiked':
+        return <MostLikedRecipes />;
+      case 'topRated':
+        return <MostAverageRatingRecipes />;
+      case 'prepTime':
+        return <SortRecipeByPreparationTime />;
+      default:
+        return <LatestRecipes />;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollView}>
@@ -29,7 +61,7 @@ const HomeScreen = () => {
         {/* filter and sorting */}
         <View style={styles.iconsContainer}>
           <Icon name="filter" size={30} color="black" onPress={() => console.log('Filter pressed')} />
-          <Icon name="sort" size={30} color="black" onPress={() => console.log('Sort pressed')} />
+          <Icon name="sort" size={30} color="black" onPress={openSortModal} />
         </View>
 
        {/* Search Bar */}
@@ -47,7 +79,7 @@ const HomeScreen = () => {
           </View>
         </View>
 
-        {/* Categories and Chefs container */}
+        
         <View style={styles.categoriesAndChefsContainer}>
           {/* Categories */}
           <View style={styles.categoriesContainer}>
@@ -57,7 +89,6 @@ const HomeScreen = () => {
             </TouchableOpacity>
           </View>
 
-          {/* Your Categories Component */}
           <Categories />
         </View>
 
@@ -72,12 +103,43 @@ const HomeScreen = () => {
           <Chefs />
         </View>
 
-        {/* Latest Recipes */}
+        {/* Latest or Most Liked or Top Rated Recipes or by preparation time based on sorting option */}
         <View style={styles.latestRecipesContainer}>
-          <Text style={styles.boldText}>Latest Recipes</Text>
-          <LatestRecipes />
+          {renderRecipesComponent()}
         </View>
       </ScrollView>
+
+      {/* Sort Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalTitle}>Sort by:</Text>
+              <TouchableOpacity onPress={() => handleSortingOptionChange('latest')}>
+                <Text style={styles.sortOption}>Latest</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleSortingOptionChange('mostLiked')}>
+                <Text style={styles.sortOption}>Most Liked</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleSortingOptionChange('topRated')}>
+                <Text style={styles.sortOption}>Top Rated</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleSortingOptionChange('prepTime')}>
+                <Text style={styles.sortOption}>Preparation Time</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleSortingOptionChange('latest')}>
+                <Text style={styles.sortOption}>Newest</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
 
       {/* Footer */}
       <Footer/>
@@ -141,6 +203,27 @@ const styles = StyleSheet.create({
   },
   latestRecipesContainer: {
     marginTop: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  sortOption: {
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
 
